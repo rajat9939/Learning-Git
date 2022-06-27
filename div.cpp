232bbd06f38347154c1816ab89fcc39a7e97fd08
Rajat Kumar <rajatkumar9939@gmail.com>
2022-06-28 01:33:57 +0530

    Added div.cpp

---
+++ div.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+       int a, b;
+       cin>>a>>b;
+       if(b!=0)
+       {
+              cout<<"Division of two number: "<<a/b<<endl;
+       }
+       else
+       {
+              cout<<"Error: Denominator is zero"<<endl;
+       }
+       return 0;
+}
